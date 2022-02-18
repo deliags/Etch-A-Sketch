162e11304ee7b1758562d60ca754c00a4d2411da
deliags <deliagsmith@outlook.com>
2022-02-18 13:48:44 +0000

    added hover functionality (change color on mouseover)

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const container = document.querySelector(".container");
 
-function createGrid (amount) {
+//creates a grid with the given amount of rows/columns
+const createGrid = (amount) => {
   for (let i = 0; i < amount; i++){
     for (let j = 0; j < amount; j++) {
       const gridRow = document.createElement('div');
       gridRow.classList.add('grid');
+      gridRow.setAttribute('onmouseover', 'changeColor(this)');
       container.appendChild(gridRow);
-    }
-  }
+    };
+  };
 };
+
+//changes color of div when mouse hovers on it 
+const changeColor = (square) => {
+  square.style.backgroundColor = "black";
+}
+
 createGrid(16);
