10c550c7e57abbaddf1867a529220f82ef1b8697
deliags <deliagsmith@outlook.com>
2022-02-18 15:35:04 +0000

    added buttons controls

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const clearButton = document.querySelector(".clear-grid");
 
 //creates a grid with the given amount of rows/columns
 const createGrid = (amount) => {
+  
   for (let i = 0; i < amount; i++){
     for (let j = 0; j < amount; j++) {
       const gridRow = document.createElement('div');
@@ -16,14 +17,22 @@ const createGrid = (amount) => {
 //changes color of div when mouse hovers on it 
 const changeColor = (square) => {
   square.style.backgroundColor = "black";
-}
-
+};
+  
 //Refreshes page
 const clearGrid = () => {
   location.reload();
+};
+
+// //When button is clicked, page is refreshed
+// clearButton.addEventListener('click', clearGrid);
+
+// createGrid(16);
+
+const startGame = () => {
+  //When button is clicked, page is refreshed
+  clearButton.addEventListener('click', clearGrid);
+  createGrid(16)
 }
 
-//When button is clicked, page is refreshed
-clearButton.addEventListener('click', clearGrid);
-
-createGrid(16);
+startGame();
