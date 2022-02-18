1f5965fa17f825b120b858983d30ca3c0dde6e87
deliags <deliagsmith@outlook.com>
2022-02-18 17:47:06 +0000

    clear board when chosing new size

--- app.js
+++ app.js
@@ -4,22 +4,29 @@ const smallButton = document.querySelector(".small");
 const mediumButton = document.querySelector(".medium");
 const bigButton = document.querySelector(".big");
 
+//Refreshes page
+const clearGrid = () => {
+  // location.reload();
+
+  let gridPixels = container.querySelectorAll('div');
+    gridPixels.forEach(gridPixel => gridPixel.remove());
+};
+
 //creates a grid with the given amount of rows/columns
 const createGrid = (size) => {
-  
   for (let i = 0; i < size; i++){
     for (let j = 0; j < size; j++) {
       const gridRow = document.createElement('div');
       gridRow.setAttribute('onmouseover', 'changeColor(this)');
 
       if (size === 16) {
-        gridRow.classList.add('big-grid');
+        gridRow.classList.add('big-grid', 'grid');
       }
       if (size === 32) {
-        gridRow.classList.add('medium-grid');
+        gridRow.classList.add('medium-grid', 'grid');
       }
       if(size === 64){
-        gridRow.classList.add('small-grid');
+        gridRow.classList.add('small-grid', 'grid');
       }
       // gridRow.classList.add('.grid')
       container.appendChild(gridRow);
@@ -32,17 +39,14 @@ const changeColor = (square) => {
   square.style.backgroundColor = "black";
 };
   
-//Refreshes page
-const clearGrid = () => {
-  location.reload();
-};
+
 
 const startGame = () => {
   //When button is clicked, page is refreshed
   clearButton.addEventListener('click', clearGrid);
-  smallButton.addEventListener('click', () => { createGrid(16) });
-  mediumButton.addEventListener('click', () => { createGrid(32) });
-  bigButton.addEventListener('click', () => { createGrid(64) });
+  smallButton.addEventListener('click', () => { clearGrid(), createGrid(64) });
+  mediumButton.addEventListener('click', () => { clearGrid(), createGrid(32) });
+  bigButton.addEventListener('click', () => { clearGrid(), createGrid(16) });
 }
 
 startGame();
