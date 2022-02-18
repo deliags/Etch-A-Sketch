9591a1ba3d1a09393290f703e79263e1c3c7d623
deliags <deliagsmith@outlook.com>
2022-02-18 16:43:55 +0000

    added different sizes

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const container = document.querySelector(".container");
 const clearButton = document.querySelector(".clear-grid");
+const smallButton = document.querySelector(".small");
+const mediumButton = document.querySelector(".medium");
+const bigButton = document.querySelector(".big");
 
 //creates a grid with the given amount of rows/columns
-const createGrid = (amount) => {
+const createGrid = (size) => {
   
-  for (let i = 0; i < amount; i++){
-    for (let j = 0; j < amount; j++) {
+  for (let i = 0; i < size; i++){
+    for (let j = 0; j < size; j++) {
       const gridRow = document.createElement('div');
-      gridRow.classList.add('grid');
       gridRow.setAttribute('onmouseover', 'changeColor(this)');
+
+      if (size === 16) {
+        gridRow.classList.add('big-grid');
+      }
+      if (size === 32) {
+        gridRow.classList.add('medium-grid');
+      }
+      if(size === 64){
+        gridRow.classList.add('small-grid');
+      }
+      // gridRow.classList.add('.grid')
       container.appendChild(gridRow);
     };
   };
@@ -24,15 +37,12 @@ const clearGrid = () => {
   location.reload();
 };
 
-// //When button is clicked, page is refreshed
-// clearButton.addEventListener('click', clearGrid);
-
-// createGrid(16);
-
 const startGame = () => {
   //When button is clicked, page is refreshed
   clearButton.addEventListener('click', clearGrid);
-  createGrid(16)
+  smallButton.addEventListener('click', () => { createGrid(16) });
+  mediumButton.addEventListener('click', () => { createGrid(32) });
+  bigButton.addEventListener('click', () => { createGrid(64) });
 }
 
 startGame();
